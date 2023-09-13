5e5dce28e2561a73d1bcaef4bc26e7980b4a5a28
bambery <lwszolek@gmail.com>
2023-09-13 10:55:33 -0500

    the requirements specify throwing an error, so let's throw an error

--- answer.js
+++ answer.js
@@ -1,6 +1,6 @@
 export const rotate = (arr, pos) => {
     if (pos < 0) {
-        return false
+        throw new Error('Rotations cannot be negative')
     }
     pos = pos % arr.length
     return arr.slice(pos).concat(arr.slice(0, pos))
